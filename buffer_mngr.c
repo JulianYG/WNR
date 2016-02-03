165ab81d4963cbf935a70fbc4413f1095a3cb086
Julian <yg1811@hotmail.com>
2016-02-03 00:05:41 -0600

    fixed errors

--- buffer_mngr.c
+++ buffer_mngr.c
@@ -7,37 +7,38 @@ void buffer_manager_init(buffer_manager* bm, size_t max_data_cap, size_t item_si
 	bm->num_channel = channel_cnt;
 
 	for (int i = 0; i < channel_cnt; ++i) {
-		buffer_init(*bm->c_buf[i], max_data_cap + max_data_cap / 16 + 64 + 3);
+		buffer_init(&bm->c_buf[i], max_data_cap + max_data_cap / 16 + 64 + 3, sizeof(uint8_t));
 	}
 }
 
 void buffer_manager_free(buffer_manager* bm)
 {
 	for (int i = 0; i < bm->num_channel; ++i) {
-		buffer_free(*bm->c_buf[i]);
+		buffer_free(&bm->c_buf[i]);
 	}
 	free(bm->c_buf);
 }
 
 int buffer_manager_handler(buffer_manager* bm, wnr_data *item)
-{
-	if ((int k = buffer_compress(item->data, item->size, bm->c_buf[item->channel_num])) != BUFFER_SUCCESS) {
+{	
+	int k = buffer_compress(item->data, item->size, &bm->c_buf[item->channel_num]);
+	if (k != BUFFER_SUCCESS) {
 		// if not successful, first send out all remaining data
-		wnr_data toSend = {.data = bm->c_buf[item->channel_num], 
-			.size = sizeof(*(bm->c_buf[item->channel_num])), .channel_num = item->channel_num};
+		wnr_data toSend = {.data = bm->c_buf[item->channel_num].buffer, 
+			.size = sizeof(bm->c_buf[item->channel_num]), .channel_num = item->channel_num};
 	
 		buffer_manager_send(&toSend);
 
 		// then keep pushing in the rest (pointer arithmetic)
-		buffer_compress(item->data + k, item->size - k, bm->c_buf[item->channel_num]);
+		buffer_compress(item->data + k, item->size - k, &bm->c_buf[item->channel_num]);
 		// just let it be lost if still not successful
 	}
      
 		return -1;
 		// if buffer full, then reset the buffer
 
-	wnr_data toSend = {.data = bm->c_buf[item->channel_num], 
-		.size = sizeof(*(bm->c_buf[item->channel_num])), .channel_num = item->channel_num};
+	wnr_data toSend = {.data = bm->c_buf[item->channel_num].buffer, 
+		.size = sizeof(bm->c_buf[item->channel_num]), .channel_num = item->channel_num};
 	
 	buffer_manager_send(&toSend);
 
