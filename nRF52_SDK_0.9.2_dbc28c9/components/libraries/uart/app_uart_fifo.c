390354206f6e166b609c7d71e30dd1689dc9227c
Julian <yg1811@hotmail.com>
2016-02-26 18:05:59 -0500

    back

--- nRF52_SDK_0.9.2_dbc28c9/components/libraries/uart/app_uart_fifo.c
+++ nRF52_SDK_0.9.2_dbc28c9/components/libraries/uart/app_uart_fifo.c
@@ -166,46 +166,44 @@ uint32_t app_uart_get(uint8_t * p_byte)
 
 uint32_t app_uart_compress(uint8_t byte)
 {
-//		printf("%uhh", byte);
     if (buffer_count >= 64) {
-			heatshrink_encoder *hse = heatshrink_encoder_alloc(10, 3);
-			size_t comp_sz = 64 + (64 / 2) + 4;
-			uint8_t *comp = malloc(comp_sz);
-			memset(comp, 0, comp_sz);
-			size_t count = 0;
-			uint32_t sunk = 0;
-			uint32_t polled = 0;
+		heatshrink_encoder *hse = heatshrink_encoder_alloc(10, 3);
+		size_t comp_sz = 64 + (64 / 2) + 4;
+		uint8_t *comp = malloc(comp_sz);
+		memset(comp, 0, comp_sz);
+		size_t count = 0;
+		uint32_t sunk = 0;
+		uint32_t polled = 0;
 			
-			while (sunk < 64) {
-        heatshrink_encoder_sink(hse, &data_buf, 64 - sunk, &count);
-        sunk += count;
-				printf("sunk %zu", sunk);
-				if (sunk == 64)
+		while (sunk < 64) {
+	        heatshrink_encoder_sink(hse, &data_buf[sunk], 64 - sunk, &count);
+	       	sunk += count;
+			printf("sunk %zu", sunk);
+			if (sunk == 64)
 				heatshrink_encoder_finish(hse);
-				
-        HSE_poll_res pres;
-        do {                    /* "turn the crank" */
-            pres = heatshrink_encoder_poll(hse, &comp[polled], comp_sz - polled, &count);
-            polled += count;
-        } while (pres == HSER_POLL_MORE);
-			}
-	  		printf("%zu", polled);
-      for (int i = 0; i < polled; i++) {
-					printf("%comp uhh", comp[i]);
-          while (app_uart_put(comp[i]) != NRF_SUCCESS) {
-          //    return NRF_ERROR_NOT_SUPPORTED;
-          }
-      }
-			
-      buffer_count = 0;
-//			if (sunk == 64) {
-//					heatshrink_encoder_finish(hse);
-//			}
-			heatshrink_encoder_free(hse);
+					
+	        HSE_poll_res pres;
+	        
+	        do {                    /* "turn the crank" */
+	            pres = heatshrink_encoder_poll(hse, &comp[polled], comp_sz - polled, &count);
+	            polled += count;
+	        } while (pres == HSER_POLL_MORE);
+		}
+		printf("%zu", polled);
+	    for (int i = 0; i < polled; i++) {
+			printf("%comp uhh", comp[i]);
+	        while (app_uart_put(comp[i]) != NRF_SUCCESS) {};
+	          //    return NRF_ERROR_NOT_SUPPORTED;
+	    }
+
+	    buffer_count = 0;
+	//			if (sunk == 64) {
+	//					heatshrink_encoder_finish(hse);
+	//			}
+		heatshrink_encoder_free(hse);
     }
     data_buf[buffer_count] = byte;
     buffer_count++;
-
     return NRF_SUCCESS;
 }
 
